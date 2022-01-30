e32f96550809db57b10e8ec7ce0f0654907ea324
Anastasia Solovei <av.ignatyuk@gmail.com>
2022-01-30 14:04:49 +0200

    fix bcg images

--- src/pages/FeedbackPage/FeedbackPage.js
+++ src/pages/FeedbackPage/FeedbackPage.js
@@ -4,16 +4,19 @@ import styled from 'styled-components';
 import FeedbackForm from '../../components/FeedbackForm';
 import SocialList from '../../components/SocialList';
 
+import cloudsImg from '../../images/clouds.png';
+import ellipseImg from '../../images/yellow-ellipse.png';
+import yellowSmallImg from '../../images/yellow_small.png';
+import yellowBigImg from '../../images/yellow_big.png';
+import pinkRightImg from '../../images/pink-right.png';
+import pinkLeftImg from '../../images/pink-left.png';
+import greenRightImg from '../../images/green.png';
+import mapImg from '../../images/map.png';
+
 const Container = styled.div`
   width: 1440px;
   margin-right: auto;
   margin-left: auto;
-`;
-
-const Main = styled.main`
-  background-repeat: no-repeat;
-  background-position: right 63px top 101px;
-  background-image: url('../../images/clouds.png');
 `;
 
 const Content = styled.section`
@@ -24,10 +27,11 @@ const Content = styled.section`
 
   background-repeat: no-repeat;
   background-position: left 714px top 21px, left 29px top 23px,
-    left 763px top 516px, left 762px top 558px, right 0px top 0px;
-  background-image: url('../../images/yellow-ellipse.png'),
-    url('../../images/yellow_lefy.png'), url('../../images/yellow_right.png'),
-    url('../../images/pink-right.png'), url('../../images/map.png');
+    left 763px top 516px, left 762px top 558px, right 0px top 0px,
+    right 63px top 101px;
+  background-image: url(${ellipseImg}), url(${yellowSmallImg}),
+    url(${yellowBigImg}), url(${pinkRightImg}), url(${mapImg}),
+    url(${cloudsImg});
 `;
 
 const Heading = styled.h1`
@@ -46,19 +50,18 @@ const Footer = styled.footer`
   background-repeat: no-repeat;
   background-position: left 15px bottom 0px, right 127px bottom 103px,
     left 1356px bottom 28px;
-  background-image: url('../../images/pink-left.png'),
-    url('../../images/green-right.png'), url('../../images/yellow_lefy.png');
+  background-image: url(${pinkLeftImg}), url(${greenRightImg}),
+    url(${yellowSmallImg});
 `;
 
 export default function FeedbackPage() {
   return (
     <Container>
-      <Main>
-        <Content>
-          <Heading>Reach out to us!</Heading>
-          <FeedbackForm />
-        </Content>
-      </Main>
+      <Content>
+        <Heading>Reach out to us!</Heading>
+        <FeedbackForm />
+      </Content>
+
       <Footer>
         <SocialList />
       </Footer>
